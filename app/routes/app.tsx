095769fb22db40fa782e3af753958b736f8d67e9
conradkoh <conradkoh@gmail.com>
2024-04-13 08:20:57 +0800

    added dead code example

--- app/routes/app.tsx
+++ app/routes/app.tsx
@@ -1,6 +1,8 @@
 import { Outlet } from '@remix-run/react';
 import { SessionProvider } from 'convex-helpers/react/sessions';
 export default function Layout() {
+  return <div>hi</div>;
+  // below is dead code, it doesn't get executed but still causes this to error out
   return (
     <div>
       <SessionProvider>
